ddc437d0f5793180fc48650d687ddb3b93c14dbb
Manu Chaudhary <manu_talkin@yahoo.com>
2016-03-02 17:33:23 +0530

    Completed Styles and Themes

--- app/src/main/java/com/example/manuchaudhary/coolweather/ForecastAdapter.java
+++ app/src/main/java/com/example/manuchaudhary/coolweather/ForecastAdapter.java
@@ -92,13 +92,18 @@ public class ForecastAdapter extends CursorAdapter {
 
         // Read date from cursor
         long dateInMillis = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+
         // Find TextView and set formatted date on it
         viewHolder.dateView.setText(Utility.getFriendlyDayString(context, dateInMillis));
 
         // Read weather forecast from cursor
         String description = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+
         // Find TextView and set weather forecast on it
         viewHolder.descriptionView.setText(description);
+
+        // For accessibility, add a content description to the icon field
+        viewHolder.iconView.setContentDescription(description);
 
         // Read user preference for metric or imperial temperature units
         boolean isMetric = Utility.isMetric(context);
